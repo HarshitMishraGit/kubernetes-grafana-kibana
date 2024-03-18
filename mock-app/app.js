ea0851b56e148a2d94d77666329a3720bf906d0a
Harshit Mishra <harshitm@Harshits-MacBook-Pro.local>
2024-03-18 12:49:27 +0530

    add prom-client

--- mock-app/app.js
+++ mock-app/app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var clientProvider = require('prom-client')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+var collectDefaultMetrics = clientProvider.collectDefaultMetrics;
+collectDefaultMetrics({ });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,7 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/metrics',async (req, res) => {
+  res.set('Content-Type', clientProvider.register.contentType);
+  const metrics = await clientProvider.register.metrics();
+  res.end(metrics);
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
